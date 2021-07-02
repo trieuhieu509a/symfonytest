803700f0600473f00d080b01f5f0278c63453a12
hieu <htrinhtrieu@pentalog.com>
2021-07-03 00:04:23 +0700

    install fos rest bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
---
+++ src/Controller/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractFOSRestController
+{
+//    trait ControllerTrait
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/user/some_route", name="user add")
+     */
+    public function add(): Response
+    {
+        return $this->json([
+            'message' => 'user add',
+        ]);
+    }
+
+    /**
+     * @Route("/user/get", name="get user")
+     */
+    public function getUsersAction()
+    {
+        $data = [1,2,3]; // get data, in this case list of users.
+        $view = $this->view($data, 200);
+        var_dump($view->getContext());die;
+        return $this->handleView($view);
+    }
+
+    public function redirectAction()
+    {
+        $view = $this->redirectView($this->generateUrl('some_route'), 301);
+        // or
+        $view = $this->routeRedirectView('some_route', array(), 301);
+
+        return $this->handleView($view);
+    }
+}
